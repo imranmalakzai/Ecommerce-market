a42742586223186ee621a2551bc5a474867a8220
Imran Malakzai <afghanimran92@gmail.com>
2025-02-19 09:32:10 +0430

    Accept

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container bg-red-500'><h1 className='bg-blend-difference'>Hello Tailwind</h1></div>
+  )
+}
+
+export default App
