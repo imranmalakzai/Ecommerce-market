b4db3e09196b39000912b072d119eded7978d781
Imran Malakzai <afghanimran92@gmail.com>
2025-02-23 16:19:45 +0430

    Accept

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { registerLicense } from '@syncfusion/ej2-base'
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF1cW2hIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEBjW35XcHRUQ2RaWE1+Wg==');  // Replace with your actual key
+import './index.css'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <p className="text-blue-700">Welcome to Afghanistan</p>
+  </StrictMode>,
+)
