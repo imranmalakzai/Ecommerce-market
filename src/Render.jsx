bcaac5a939f29db653a6831c96c2689c7f627f81
Imran Malakzai <afghanimran92@gmail.com>
2025-03-02 05:06:13 +0430

    Accept

--- src/Render.jsx
+++ src/Render.jsx
@@ -1,6 +1,6 @@
 import styles from "./style"
 
-import { Billing,CardDeal,Clients,CAT,Footer,Hero,Navbar,States,Testimonials, Business } from "./components";
+import { Billing,CardDeal,Clients,CAT,Footer,Hero,Navbar,States,Testimonials, Business } from "./components/index";
 
 const Render = () => (
   <div className="w-full bg-black overflow-hidden">
